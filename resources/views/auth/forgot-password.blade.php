7f2e08eb560595cc21a27f1c90e3be642c1c55af
joytimess <victorw7286@gmail.com>
2026-03-04 09:17:04 +0700

    add table to sales blade

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,70 +1,33 @@
 @extends('layouts.auth')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-xl-10 col-lg-12 col-md-9">
-            <div class="card o-hidden border-0 shadow-lg my-5">
-                <div class="card-body p-0">
-                    <div class="row">
-                        <div class="col-lg-6 d-none d-lg-block bg-password-image"></div>
-                        <div class="col-lg-6">
-                            <div class="p-5">
-                                <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-2">Forgot Your Password?</h1>
-                                    <p class="mb-4">We get it, stuff happens. Just enter your email address below and we'll send you a link to reset your password!</p>
-                                </div>
-                                @include('partials.flash')
-                                <form class="user" method="POST" action="{{ route('password.email') }}">
-                                    @csrf
-                                    <div class="form-group">
-                                        <input type="email" name="email" class="form-control form-control-user" id="email" placeholder="Enter Email Address..." required>
-                                    </div>
-                                    <button type="submit" class="btn btn-primary btn-user btn-block">Reset Password</button>
-                                </form>
-                                <hr>
-                                {{-- <div class="text-center">
-                                    <a class="small" href="#">Create an Account!</a>
-                                </div> --}}
-                                <div class="text-center">
-                                    <a class="small" href="{{ route('login') }}">Already have an account? Login!</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+    <div class="min-h-screen! flex! items-center! justify-center! bg-[#E7F3EE]!">
+        <div class="p-10! bg-white! rounded-xl shadow-sm max-w-[425px]">
+            
+            {{-- LOGO DAXTRO --}}
+            <div class="flex justify-center">
+                <img src="{{ asset('assets/images/logo.png') }}" style="max-height: 60px;" alt="Logo Daxtro">
+            </div>
+
+            {{-- ACCENT TEXT --}}
+            <h1 class="text-2xl! font-bold text-[#1E1E1E] mt-10">Forgot Your Password?</h1>
+            <p class="text-[#757575] text-base! mt-1">Please enter your email address below and we'll send you a link to reset your password!</p>
+
+            @include('partials.flash')
+
+            <form class="user" method="POST" action="{{ route('password.email') }}">
+                @csrf
+                <div class="form-group py-5">
+                    <label for="email" class="text-[#1E1E1E]">Email</label>    
+                    <input type="email" name="email" class="w-full rounded-lg px-3 py-2 border border-[#D9D9D9] focus:outline-[#115640]" id="email" placeholder="Enter Email Address..." required>
                 </div>
-            </div>
+                <button type="submit" class="bg-[#115640] w-full rounded-lg text-white py-3 cursor-pointer">Reset Password</button>
+            </form>
+                    
+            <a class="small" href="{{ route('login') }}">
+                <div class="w-full bg-white text-[#1E1E1E] text-center border border-[#D9D9D9] py-3 rounded-lg mt-3">
+                    Go Back to Login
+                </div>
+            </a>
         </div>
     </div>
-</div>
-
-<div class="min-h-screen! flex! items-center! justify-center! bg-[#E7F3EE]!">
-    <div class="p-10! bg-white! rounded-xl shadow-sm max-w-[425px]">
-        
-        {{-- LOGO DAXTRO --}}
-        <div class="flex justify-center">
-            <img src="{{ asset('assets/images/logo.png') }}" style="max-height: 60px;" alt="Logo Daxtro">
-        </div>
-
-        {{-- ACCENT TEXT --}}
-        <h1 class="text-2xl! font-bold text-[#1E1E1E] mt-10">Forgot Your Password?</h1>
-        <p class="text-[#757575] text-base! mt-1">Please enter your email address below and we'll send you a link to reset your password!</p>
-
-        @include('partials.flash')
-
-        <form class="user" method="POST" action="{{ route('password.email') }}">
-            @csrf
-            <div class="form-group py-5">
-                <label for="email" class="text-[#1E1E1E]">Email</label>    
-                <input type="email" name="email" class="w-full rounded-lg px-3 py-2 border border-[#D9D9D9] focus:outline-[#115640]" id="email" placeholder="Enter Email Address..." required>
-            </div>
-            <button type="submit" class="bg-[#115640] w-full rounded-lg text-white py-3 cursor-pointer">Reset Password</button>
-        </form>
-                
-        <a class="small" href="{{ route('login') }}">
-            <div class="w-full bg-white text-[#1E1E1E] text-center border border-[#D9D9D9] py-3 rounded-lg mt-3">
-                Go Back to Login
-            </div>
-        </a>
-    </div>
-</div>
 @endsection
--- resources/views/pages/dashboard/sales.blade.php
+++ resources/views/pages/dashboard/sales.blade.php
@@ -71,23 +71,23 @@
             <div>
                 <div class="mt-3 text-[#757575]">
                     <p id="totalLeads">0/</p>
-                    <p id="totalTrash">0</p>
+                    <p id="totalTrash">Trash Leads: 0</p>
                 </div>
 
                 <div class="flex items-center justify-start gap-3 mt-3">
                     <div class="flex items-center gap-1">
                         <span class="w-[8px] h-[8px] rounded-full block bg-[#3F80EA]"></span>
-                        <p id="coldLeads">0</p>
+                        <p id="coldLeads">0 Cold</p>
                     </div>
 
                     <div class="flex items-center gap-1">
                         <span class="w-[8px] h-[8px] rounded-full block bg-[#E8B931]"></span>
-                        <p id="warmLeads">0</p>
+                        <p id="warmLeads">0 Warm</p>
                     </div>
 
                     <div class="flex items-center gap-1">
                         <span class="w-[8px] h-[8px] rounded-full block bg-[#EC221F]"></span>
-                        <p id="warmLeads">0</p>
+                        <p id="hotLeads">0 Hot</p>
                     </div>
                 </div>
             </div>
@@ -99,7 +99,7 @@
 
             <div class="flex justify-between items-center">
                 
-                <h1 class="text-[#757575] font-semibold">Potential Dealing <span>(Warm + Hot)</span></h1>
+                <h1 class="text-[#757575] font-semibold">Potential Dealing <span class="block">(Warm + Hot)</span></h1>
                 
                 <div class="p-3 border border-[#D9D9D9] rounded-md text-[#417866]">
                     <x-icon.dollar/>
@@ -107,7 +107,7 @@
             </div>
 
             <div>
-                <div class="mt-3 text-[#757575]">
+                <div class="mt-3 text-[#757575] inline-block">
                     <p id="potentialTotalAmount">0</p>
                     <p id="potentialTotalOpportunity">0</p>
                 </div>
@@ -118,7 +118,84 @@
 
     {{-- ACTIVITY OPPORTUNITIES --}}
     <h1 class="text-[#083224] font-semibold uppercase mt-5 text-lg">My Active Opportunities</h1>
+    <div class="mt-4 bg-white rounded-lg border-r border-l border-t border-[#D9D9D9]">
+        {{-- NAVIGATION TABLES --}}
+        <div class="bg-white lg:flex justify-between items-center border-b border-[#D9D9D9] p-3 gap-4 rounded-tr-lg rounded-tl-lg sm:gap-3 grid grid-cols-1">
+            {{-- SEARCH TABLES --}}
+            <div class="lg:w-1/6 w-full border border-gray-300 rounded-lg flex items-center p-2">
+                <div class="px-2">
+                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M6.5 13C4.68333 13 3.14583 12.3708 1.8875 11.1125C0.629167 9.85417 0 8.31667 0 6.5C0 4.68333 0.629167 3.14583 1.8875 1.8875C3.14583 0.629167 4.68333 0 6.5 0C8.31667 0 9.85417 0.629167 11.1125 1.8875C12.3708 3.14583 13 4.68333 13 6.5C13 7.23333 12.8833 7.925 12.65 8.575C12.4167 9.225 12.1 9.8 11.7 10.3L17.3 15.9C17.4833 16.0833 17.575 16.3167 17.575 16.6C17.575 16.8833 17.4833 17.1167 17.3 17.3C17.1167 17.4833 16.8833 17.575 16.6 17.575C16.3167 17.575 16.0833 17.4833 15.9 17.3L10.3 11.7C9.8 12.1 9.225 12.4167 8.575 12.65C7.925 12.8833 7.23333 13 6.5 13ZM6.5 11C7.75 11 8.8125 10.5625 9.6875 9.6875C10.5625 8.8125 11 7.75 11 6.5C11 5.25 10.5625 4.1875 9.6875 3.3125C8.8125 2.4375 7.75 2 6.5 2C5.25 2 4.1875 2.4375 3.3125 3.3125C2.4375 4.1875 2 5.25 2 6.5C2 7.75 2.4375 8.8125 3.3125 9.6875C4.1875 10.5625 5.25 11 6.5 11Z" fill="#6B7786"/>
+                    </svg>
+                </div>
+                <input id="searchInput" type="text" placeholder="Search" class="w-full px-3 py-1 border-none focus:outline-[#115640] "/>
+            </div>
+        </div>
 
+        {{-- CONTENTS TABLES --}}
+        <div class="max-md:overflow-x-scroll">
+            <table class="w-full bg-white rounded-br-lg rounded-bl-lg">
+
+                {{-- HEADER TABLE --}}
+                <thead class="text-[#1E1E1E]">
+                    <tr class="border-b border-b-[#CFD5DC]">
+                        <th class="hidden">ID (hidden)</th>
+                        <th class="p-1 md:p-2 lg:p-3">
+                            Customer Name
+                        </th>
+                        <th class="p-1 md:p-2 lg:p-3">  
+                            Stage
+                        </th>
+                        <th class="p-1 md:p-2 lg:p-3">
+                            Amount
+                        </th>
+                        <th class="p-1 md:p-2 lg:p-3">
+                            Product
+                        </th>
+                        <th class="p-1 md:p-2 lg:p-3">
+                            Segment
+                        </th>
+                        <th class="p-1 md:p-2 lg:p-3">
+                            Last Activity
+                        </th>
+                        <th class="text-center p-1 md:p-2 lg:p-3">
+                            Data Validation
+                        </th>
+                    </tr>
+                </thead>
+                <tbody id="bodyTable"></tbody>
+            </table>
+            {{-- NAVIGATION ROWS --}}
+            <div class="flex justify-between items-center px-3 py-2 text-[#1E1E1E]! bg-transparent">
+                <div class="flex items-center gap-3">
+                    <p class="font-semibold">Show Rows</p>
+                    <select id="tabPageSizeSelect" class="w-auto bg-white font-semibold p-2 rounded-md"
+                        onchange="changePageSize('tab', this.value)">
+                        <option value="5">5</option>
+                        <option value="10" selected>10</option>
+                        <option value="25">25</option>
+                        <option value="50">50</option>
+                        <option value="100">100</option>
+                    </select>
+                </div>
+
+                <div class="flex items-center gap-2">
+                    <div id="tabShowing" class="font-semibold">Showing 0-0 of 0</div>
+                    <div>
+                        <button id="tabPrevBtn"
+                            class="btn btn bg-white border! border-[#D9D9D9]! cursor-pointer!"
+                            onclick="goPrev('tab')">
+                            <i class="fas fa-chevron-left text-black" style="font-size: 12px;"></i>
+                        </button>
+                        <button id="tabNextBtn" class="btn bg-white border! border-[#D9D9D9]! cursor-pointer!"
+                            onclick="goNext('tab')">
+                            <i class="fas fa-chevron-right text-black" style="font-size: 12px;"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </section>
 
@@ -143,10 +220,12 @@
 
             const data = result.Data;
 
-            const 
+            
+            const achievementSalesFormatted = formatRupiah(data.achievement_target.achievement);
+            $("#achievementSales").text(achievementSalesFormatted + "/").addClass('font-semibold text-2xl text-[#1E1E1E]');
 
-            $("#achievementSales").text(data.achievement_target.achievement + "/");
-            $("#targetSales").text(data.achievement_target.target);
+            const targetSalesFormatted = formatRupiah(data.achievement_target.target);
+            $("#targetSales").text(targetSalesFormatted).addClass('font-semibold text-2xl text-[#1E1E1E]');
             
             if ( data.achievement_target.percentage > 70 ){
                 $("#percentageAchievement").text(data.achievement_target.percentage + "%").addClass('font-semibold! status-finish');
@@ -157,27 +236,31 @@
                 $("#percentageAchievement").text(data.achievement_target.percentage + "%").addClass('font-semibold! status-expired');
             }
 
-            $("#totalDeals").text(data.closed_deal.total_deals + "/");
+            $("#totalDeals").text(data.closed_deal.total_deals).addClass('font-semibold text-3xl text-[#1E1E1E]');;
 
             const totalAmountFormatted = formatRupiah(data.closed_deal.total_amount);
-            $("#totalAmount").text(totalAmountFormatted);
+            $("#totalAmount").text("Amount: " + totalAmountFormatted);
 
             $("#conversionRate").text(data.closed_deal.conversion_rate + "%");
 
-            $("#totalLeads").text(data.active_leads.total + "/");
-            $("#totalTrash").text(data.active_leads.trash);
+            $("#totalLeads").text(data.active_leads.total).addClass('font-semibold text-3xl text-[#1E1E1E]');
+            $("#totalTrash").text("Trash Leads: " + data.active_leads.trash);
 
-            $("#coldLeads").text(data.active_leads.cold);
-            $("#warmLeads").text(data.active_leads.warm);
-            $("#hotLeads").text(data.active_leads.hot);
+            $("#coldLeads").text(data.active_leads.cold + ' Cold');
+            $("#warmLeads").text(data.active_leads.warm + ' Warm');
+            $("#hotLeads").text((data.active_leads?.hot ?? 0) + " Hot");
 
             const potentialAmountFormatted = formatRupiah(data.potential_dealing.total_amount);
-            $("#potentialTotalAmount").text(potentialAmountFormatted);
-            $("#potentialTotalOpportunity").text(data.potential_dealing.total_opportunity);
+            $("#potentialTotalAmount").text(potentialAmountFormatted).addClass('font-semibold text-3xl text-[#1E1E1E]');
+            $("#potentialTotalOpportunity").text(data.potential_dealing.total_opportunity + ' Active Opportunity').addClass('text-right');
 
         } catch (error) {
             console.error("Error loading dashboard grid:", error);
         }
+    }
+
+    async function loadActivity(){
+
     }
 
     function formatRupiah(number) {
